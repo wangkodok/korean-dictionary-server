6356434461e68ab9e60eb36c6b75dda07b0401ad
wangkodok <wangkodok@gmail.com>
2024-08-19 16:47:46 +0900

    불필요한 코드 제거

--- app.js
+++ app.js
@@ -10,15 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-const externalApiUrl = `https://stdict.korean.go.kr/api/search.do?certkey_no=6715&key=${API_KEY}&type_search=search&req_type=json&q=`;
+const externalApiUrl = `https://stdict.korean.go.kr/api/search.do?certkey_no=6715&key=${API_KEY}&type_search=search&req_type=json&q=나무`;
 console.log("External API URL:", externalApiUrl);
-
-if (!API_KEY) {
-  console.error(
-    "API_KEY is not set. Please set your API_KEY in the environment variables."
-  );
-  process.exit(1); // 서버 실행을 중단하고 오류를 발생시킵니다.
-}
 
 app.get("/", (req, res) => {
   res.send("메인");
