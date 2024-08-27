01f2102cc133423bfb5f94a8009f6a90bd266bff
wangkodok <wangkodok@gmail.com>
2024-08-27 20:17:50 +0900

    url 수정

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-const externalApiUrl = `https://stdict.korean.go.kr/api/search.do?certkey_no=6715&key=${API_KEY}&type_search=search&req_type=json&q=`;
-console.log("External API URL:", externalApiUrl);
+const externalApiUrl = `https://stdict.korean.go.kr/api/search.do?key=${API_KEY}&type_search=search&req_type=json&q=`;
 
 app.get("/", (req, res) => {
   res.send("메인");
@@ -22,7 +21,9 @@ app.get("/fetch-data", async (req, res) => {
   const { query } = req.query;
 
   try {
+    // console.log("External API URL:", externalApiUrl);
     const response = await axios.get(`${externalApiUrl}${query}`);
+    // console.log(response.data.channel.item);
     res.json(response.data);
   } catch (error) {
     res.status(500).send("Error fetching data from external API");
